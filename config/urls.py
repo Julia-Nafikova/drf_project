7aa4770b450cf5d15bcf250c6df56066ce24c2f1
Nafikova_Julia <nafikova91@yandex.ru>
2025-03-16 14:33:20 +0300

    add filters for payment

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('lms.urls'), name='lms'),
+    path('', include('users.urls'), name='users'),
 ]
