72cc2aa2c0afe0c203344cb44f1596421f299816
Nafikova_Julia <nafikova91@yandex.ru>
2025-03-21 13:03:21 +0300

    install and connect simplejwt

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_simplejwt'
     'django_filters',
     'rest_framework',
     'users',
@@ -146,4 +147,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
+
