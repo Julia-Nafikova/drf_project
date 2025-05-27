ec3cf377515cd885bbd7b8db6693e125627a7605
Nafikova_Julia <nafikova91@yandex.ru>
2025-05-27 13:18:59 +0300

    test deploy

--- config/settings.py
+++ config/settings.py
@@ -170,8 +170,8 @@ REST_FRAMEWORK = {
 
 STRIPE_APIKEY = os.getenv('STRIPE_APIKEY')
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 
 CELERY_TIMEZONE = TIME_ZONE
 
